8e3c940b32a49995fda56b510e583256ec361621
jahnvi655 <245123751040@mvsrec.edu.in>
2026-04-15 13:16:09 +0530

    Updated Armstrong program (Jenkins compatible)

--- Armstrong.java
+++ Armstrong.java
@@ -1,17 +1,16 @@
-import java.util.Scanner;
-
 public class Armstrong {
     public static void main(String[] args) {
-        int number, originalNumber, remainder, result = 0;
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        number = sc.nextInt();
+        int number = 153;
+        int originalNumber, remainder, result = 0;
+
         originalNumber = number;
+
         while (originalNumber != 0) {
             remainder = originalNumber % 10;
             result += Math.pow(remainder, 3);
             originalNumber /= 10;
         }
+
         if (result == number)
             System.out.println(number + " is an Armstrong number.");
         else
