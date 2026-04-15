5b07027962eaeb19b297e3443feae7dd72cb1bd8
jahnvi655 <245123751040@mvsrec.edu.in>
2026-04-15 13:12:07 +0530

    Added Armstrong Java program

---
+++ Armstrong.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        int number, originalNumber, remainder, result = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        number = sc.nextInt();
+        originalNumber = number;
+        while (originalNumber != 0) {
+            remainder = originalNumber % 10;
+            result += Math.pow(remainder, 3);
+            originalNumber /= 10;
+        }
+        if (result == number)
+            System.out.println(number + " is an Armstrong number.");
+        else
+            System.out.println(number + " is NOT an Armstrong number.");
+    }
+}
